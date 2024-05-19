6d8fc52b72bf8fb881d19c0a08460ee943788c26
henriquejhas <hjhas92@gmail.com>
2024-05-19 16:51:36 -0300

    adionando o terceiro curso

---
+++ Python avance na Orientação a Objetos e consuma API/oo-sabor-express/app.py
@@ -0,0 +1,17 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
+
+restaurante_praca = Restaurante('Praça','Gourmet')
+bebida_suco = Bebida('Suco de Melancia', 5.00, 'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
+def main():
+    restaurante_praca.exibir_cardapio
+
+if __name__ == '__main__':
+    main() 
